3afe765daef16147ece7ea18cc377f3d8e5742ce
Guilherme Prata Costa <guilherme.costa@invillia.com>
2024-02-14 22:36:07 -0300

    Initial Commit

---
+++ StandfordSwiftUI/StandfordSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandfordSwiftUIApp.swift
+//  StandfordSwiftUI
+//
+//  Created by Guilherme Prata Costa on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct StandfordSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
